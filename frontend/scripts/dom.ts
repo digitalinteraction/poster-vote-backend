c48d320f6dc780b671c30a5b0512698f6e449a58
Rob Anderson <rob@andrsn.uk>
2018-11-15 20:15:40 +0000

    :alembic: Experiment with data binding

--- frontend/scripts/dom.ts
+++ frontend/scripts/dom.ts
@@ -95,63 +95,61 @@ export class State<T> {
   // }
 }
 
-// export function makeState<S>(initial: S) {
-//
-//   let bound: S = {} as any
-//   Object.defineProperty(bound, 'state', {
-//     writable: true,
-//     enumerable: false,
-//     value: Object.assign({}, initial)
-//   })
-//
-//   for (let key in initial) {
-//     Object.defineProperty(bound, key, {
-//       get () {
-//         return this.state
-//       },
-//       set (v) {
-//         this.state[key] = v
-//         update(elem, this)
-//       }
-//     })
-//   }
-// }
+type SomeKey = string | number | Symbol
 
-export function bind2<E extends Element, S>(
-  id: string,
-  initial: S,
-  update: (elem: E, state: S) => void
-) {
-  const elem: E = document.getElementById(id) as any
-  if (!elem) throw new Error(`Invalid element #${id}`)
+type EffectMap<T> = { [K in keyof T]: Effect[] }
+type Effect<T = any> = (state: T) => void
 
-  // const s0: S = {} as any
-  // for (let key in initial) {
-  //   if (typeof initial[key] !== 'function') s0[key] = initial[key] as any
-  // }
+type Observed<T extends object> = T & {
+  nextEffect?: Effect<T>
+  effects: { [idx: string]: Effect[] }
+}
 
-  // let s0 = Object.assign({}, initial)
+function makeState<T extends object>(initial: T): T {
+  // Create an object to store effects
+  let effects: EffectMap<T> = {} as any
+  for (let key in initial) effects[key] = []
 
-  // let reload
+  // Create our new state, with the effects
+  let state: Observed<T> = Object.assign({ effects }, initial)
 
-  let bound: S = {} as any
-  Object.defineProperty(bound, 'state', {
-    writable: true,
-    enumerable: false,
-    value: Object.assign({}, initial)
+  // Create a proxy to register effects
+  let proxy = new Proxy<Observed<T>>(state, {
+    get(target: any, prop) {
+      // Register the effect
+      if (state.nextEffect && prop !== 'nextEffect') {
+        target.effects[prop as any].push(target.nextEffect)
+        state.nextEffect = undefined
+      }
+      return target[prop]
+    },
+    set(target, prop, value) {
+      ;(target as any)[prop] = value
+
+      // Trigger effects
+      if (target.effects[prop as any]) {
+        target.effects[prop as any].forEach(effect => effect(target))
+      }
+      return true
+    }
   })
 
-  for (let key in initial) {
-    Object.defineProperty(bound, key, {
-      get() {
-        return this.state
-      },
-      set(v) {
-        this.state[key] = v
-        update(elem, this)
-      }
-    })
-  }
-
-  return [elem, bound]
+  return proxy
 }
+
+function useEffect<T extends object>(state: T, effect: Effect<T>) {
+  ;(state as Observed<T>).nextEffect = effect
+  effect(state)
+}
+
+let s0 = makeState({ name: 'Geoff', canSubmit: false })
+
+useEffect(s0, state => {
+  state.canSubmit = state.name.length > 4
+})
+
+useEffect(s0, state => console.log(state.name, state.canSubmit))
+
+s0.name = 'Geoffrey'
+s0.name = 'Jim'
+s0.name = 'Tom'
