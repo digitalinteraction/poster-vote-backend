d717f5df821e5e73e0fc510fb90a3b2f8116a989
Rob Anderson <rob@andrsn.uk>
2018-11-15 00:28:16 +0000

    :alembic: Data binding experiment

--- frontend/scripts/dom.ts
+++ frontend/scripts/dom.ts
@@ -40,7 +40,118 @@ export function bind<E extends Element, S extends object>(
   return [elem, boundState]
 }
 
-export function bind2<E extends Element, S>(id: string, state: S) {
+// type StateDef<T> = { [K in keyof T]: T | ((state: T) => T[K]) }
+
+type ObserverMap<S> = { [K in keyof S]: Observation<S[K]>[] }
+
+type Observation<V> = (value: V) => void
+
+type Binder<T, U> = (state: State<T>) => U
+
+// type BoundState<T, K2 extends string, V> = {
+//   [K in keyof T]: T[K],
+//   [idx, K2]: V
+// }
+
+export class State<T> {
+  private internal: T
+  public observers: ObserverMap<T> = {} as any
+
+  static create<T>(initial: T): State<T> & T {
+    return new State(initial) as any
+  }
+
+  constructor(initial: T) {
+    this.internal = Object.assign({}, initial)
+
+    for (let key in initial) {
+      this.observers[key] = []
+
+      Object.defineProperty(this, key, {
+        get() {
+          return this.internal
+        },
+        set(v) {
+          this.internal[key] = v
+          this.observeChange(key, v)
+        }
+      })
+    }
+  }
+
+  private observeChange<K extends keyof T>(key: K, value: any) {
+    this.observers[key].forEach(obs => obs(value))
+  }
+
+  public observe<K extends keyof T>(key: K, observer: Observation<T[K]>) {
+    this.observers[key].push(observer)
+  }
+
+  // public bind<K extends string, V>(
+  //   key: K,
+  //   binder: Binder<T, V>
+  // ): State<T> & { K: V } {
+  //   return this
+  // }
+}
+
+// export function makeState<S>(initial: S) {
+//
+//   let bound: S = {} as any
+//   Object.defineProperty(bound, 'state', {
+//     writable: true,
+//     enumerable: false,
+//     value: Object.assign({}, initial)
+//   })
+//
+//   for (let key in initial) {
+//     Object.defineProperty(bound, key, {
+//       get () {
+//         return this.state
+//       },
+//       set (v) {
+//         this.state[key] = v
+//         update(elem, this)
+//       }
+//     })
+//   }
+// }
+
+export function bind2<E extends Element, S>(
+  id: string,
+  initial: S,
+  update: (elem: E, state: S) => void
+) {
   const elem: E = document.getElementById(id) as any
   if (!elem) throw new Error(`Invalid element #${id}`)
+
+  // const s0: S = {} as any
+  // for (let key in initial) {
+  //   if (typeof initial[key] !== 'function') s0[key] = initial[key] as any
+  // }
+
+  // let s0 = Object.assign({}, initial)
+
+  // let reload
+
+  let bound: S = {} as any
+  Object.defineProperty(bound, 'state', {
+    writable: true,
+    enumerable: false,
+    value: Object.assign({}, initial)
+  })
+
+  for (let key in initial) {
+    Object.defineProperty(bound, key, {
+      get() {
+        return this.state
+      },
+      set(v) {
+        this.state[key] = v
+        update(elem, this)
+      }
+    })
+  }
+
+  return [elem, bound]
 }
--- frontend/scripts/login.ts
+++ frontend/scripts/login.ts
@@ -1,4 +1,20 @@
-import { on, isEmail, bind } from './dom'
+import { on, isEmail, bind, bind2, State } from './dom'
+
+const state = State.create({
+  email: '',
+  canSubmit: false
+})
+
+state.observe('email', email => {
+  console.log('Email changed', email)
+})
+
+state.email = ''
+state.email = 'a'
+state.email = 'a@'
+state.email = 'a@b'
+state.email = 'a@b.'
+state.email = 'a@b.c'
 
 // on<MouseEvent>('#loginButton', 'click', (e, elem) => {
 //   console.log('Ouch!')
@@ -12,50 +28,54 @@ import { on, isEmail, bind } from './dom'
 // let loginButton: HTMLButtonElement
 // loginEmail.disabled = !isEmail(lo)
 
-type State = {
-  email: string
-}
+// type State = {
+//   email: string
+//   canSubmit: boolean
+// }
 
-type Computed = {
-  canSubmit: boolean
-}
+// type Computed = {
+//   canSubmit: boolean
+// }
 
-const state: State = {
-  email: ''
-}
+// const state: State = {
+//   email: '',
+//   canSubmit: false
+// }
 
-const computed = (state: State): Computed => ({
-  canSubmit: isEmail(state.email)
-})
+// const [input]
 
-type InputState = { disabled: boolean; value: string }
-let [email, emailState] = bind<HTMLInputElement, InputState>(
-  'loginEmail',
-  { disabled: false, value: '' },
-  (input, state) => {
-    input.disabled = state.disabled
-    input.value = state.value
-  }
-)
+// const computed = (state: State): Computed => ({
+//   canSubmit: isEmail(state.email)
+// })
 
-type ButtonState = { disabled: boolean }
-let [button, buttonState] = bind<HTMLButtonElement, ButtonState>(
-  'loginButton',
-  { disabled: true },
-  (button, state) => {
-    button.disabled = state.disabled
-  }
-)
+// type InputState = { disabled: boolean; value: string }
+// let [email, emailState] = bind<HTMLInputElement, InputState>(
+//   'loginEmail',
+//   { disabled: false, value: '' },
+//   (input, state) => {
+//     input.disabled = state.disabled
+//     input.value = state.value
+//   }
+// )
+//
+// type ButtonState = { disabled: boolean }
+// let [button, buttonState] = bind<HTMLButtonElement, ButtonState>(
+//   'loginButton',
+//   { disabled: true },
+//   (button, state) => {
+//     button.disabled = state.disabled
+//   }
+// )
 
-email.addEventListener('input', e => {
-  emailState.value = (e.target as any).value
-  buttonState.disabled = !isEmail(email.value)
-})
-
-button.addEventListener('click', e => {
-  emailState.value = ''
-  buttonState.disabled = true
-})
+// email.addEventListener('input', e => {
+//   emailState.value = (e.target as any).value
+//   buttonState.disabled = !isEmail(email.value)
+// })
+//
+// button.addEventListener('click', e => {
+//   emailState.value = ''
+//   buttonState.disabled = true
+// })
 
 // console.log(email, emailState)
 // console.log(button, buttonState)
